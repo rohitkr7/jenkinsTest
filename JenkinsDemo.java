72b14f92ea6ec2ce9b46cf42844b6b3f159951cd
rohitkrroy7 <rohitkrroy@outlook.com>
2018-03-31 11:20:49 +0530

    Adding a new funny method in the program

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -2,6 +2,11 @@ public class JenkinsDemo {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is my first jenkin experiment!");
+		String nonSense = myFunnyMethod("bom diggy diggy bom bom");
+		System.out.println(nonSense);
 	}
 
+	public static String myFunnyMethod(String passSomethingSilly) {
+		return "Whatever u just passed is nonsense!";
+	}
 }
