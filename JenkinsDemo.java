0befe9ce9274346843f1c99c1fb2922f1ddf408f
rohitkrroy7 <rohitkrroy@outlook.com>
2018-03-31 11:39:52 +0530

    Comparing two numbers

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -4,6 +4,7 @@ public class JenkinsDemo {
 		System.out.println("hi this is my first jenkin experiment!");
 		String nonSense = myFunnyMethod("bom diggy diggy bom bom");
 		System.out.println(nonSense);
+		System.out.println(10+2==12);
 	}
 
 	public static String myFunnyMethod(String passSomethingSilly) {
